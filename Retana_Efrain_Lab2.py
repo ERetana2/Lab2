71c3c48e6ce59c69d95cda6fd9f953d47c790a5f
ERetana2 <32556373+ERetana2@users.noreply.github.com>
2020-02-05 11:42:23 -0700

    Update Retana_Efrain_Lab2.py

--- Retana_Efrain_Lab2.py
+++ Retana_Efrain_Lab2.py
@@ -12,31 +12,39 @@ import os
 def read_image(imagefile):
     # Reads image in imagefile and returns color and gray-level images
     #
-    img = (plt.imread(img_dir+file)*255).astype(int)
+    img = (plt.imread(imagefile)*255).astype(int)
     img = img[:,:,:3]  # Remove transparency channel
     img_gl = np.mean(img,axis=2).astype(int)
     return img, img_gl
 
-def display_rectangle(I,r,c,h,w): # (r,c) coordinates for rectangle, h,w for height 
+def draw_rectangle(I,r,c,h,w): # (r,c) coordinates for rectangle, h,w for height 
     # and width of the rectangle
-    read_image(I)
-    rect_points = np.array([ [r,c],[r+w,c],[r-h,c-w],[r,c-w] ])
-    plt.plot(rect_points[:,0],rect_points[:,1],linewidth = 1.0,color ='k')
+    img,img_gl = read_image(I)
+    rect_points = np.array([ [r,c],[r+w,c],[r+h,c+w],[r,c+w] ])
+    ax1.plot(rect_points[:,0],rect_points[:,1],linewidth = 2.0,color ='red')
+    ax2.plot(rect_points[:,0],rect_points[:,1],linewidth = 2.0,color ='red')
+    
+def brightest_pixel(I,r,c,h,w):
+    
 
-img_dir = '.\\solar images\\' # Directory where imagea are stored
+if __name__ == "__main__":
 
-img_files = os.listdir(img_dir)  # List of files in directory
-
-plt.close('all')
-
-for file in img_files:
-    print(file)
-    if file[-4:] == '.png': # File contains an image
-        img, img_gl = read_image(img_dir+file)
-
-        fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2)
-        ax1.imshow(img)                  #Display color image
-        ax2.imshow(img_gl,cmap='gray')   #Display gray-leval image
-        plt.show()
+    img_dir = '.\\solar images\\' # Directory where imagea are stored
+    
+    img_files = os.listdir(img_dir)  # List of files in directory
+    
+    plt.close('all')
+    
+    for file in img_files:
+        print(file)
+        if file[-4:] == '.png': # File contains an image
+            img, img_gl = read_image(img_dir+file)
+    
+            fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2)
+            ax1.imshow(img)                  #Display color image
+            ax2.imshow(img_gl,cmap='gray')   #Display gray-leval image
+            
+            draw_rectangle(img_dir+file,0,0,100,100)
+            plt.show()
 
 
